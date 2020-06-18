ab8649dc97e8db65183e14947b3c125ded02d21e
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 13:32:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,9 +25,9 @@ wow_element = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array = ["wow", "goliath", "moscato", "ambrose", "axel", "arrays!"]
+  create_an_array = ["wow", "goliath", "am", "ambrose", "axel", "arrays!"]
 
-create_an_array[3]
+create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
