24c67b91a5fd2bf574ed6bd2361af83ba7c7a617
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 13:31:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 def retrieve_element_from_index(array, index_number)
   create_an_array = ["wow", "goliath", "moscato", "ambrose", "axel", "arrays!"]
 
-create_an_array[2]
+create_an_array[3]
 end
 
 def retrieve_first_element_from_array(array)
