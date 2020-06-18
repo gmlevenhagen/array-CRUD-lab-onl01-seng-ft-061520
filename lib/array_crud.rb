99f22936638050261da4d62a02d64c3e8a16bd33
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-18 13:15:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ create_an_array = ["wow", "goliath", "moscato", "ambrose", "axel", "arrays!"]
 end
 
 def remove_element_from_start_of_array(array)
-
+create_an_array = ["wow", "goliath", "moscato", "ambrose", "axel"]
+wow_element = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
